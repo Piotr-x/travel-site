d1fcc93fd26f6fd38de4ab788b471821241021b5
Piotr Szponar <18piotrek06@gmail.com>
2018-11-30 13:41:26 +0100

    Done Header styling, without JS.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,11 +5,12 @@ const gulp = require('gulp'),
     nested = require('postcss-nested'),
     cssImport = require('postcss-import'),
     mixins = require('postcss-mixins'),
-    rename = require('gulp-rename');
+    rename = require('gulp-rename'),
+    hexrgba = require('postcss-hexrgba');
 
 gulp.task('styles', () => {
     return gulp.src('./app/assets/styles/postcss/style.pcss')
-        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
         .on('error', (err) => {
             console.error(err.toString());
         })
