3ff5983945b59453365cd66c82ea77f645e0355c
Piotr Szponar <18piotrek06@gmail.com>
2018-11-30 12:39:54 +0100

    Completed automatic sprite generation.

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,35 @@
+const gulp = require('gulp'),
+    svgSprite = require('gulp-svg-sprite'),
+    rename = require('gulp-rename'),
+    del = require('del');
+
+const config = {
+    mode: {
+        css: {
+            sprite: 'sprite.svg',
+            render: {
+                css: {
+                    template: './gulp/templates/sprite.css'
+                }
+            }
+        }
+    }
+};
+
+gulp.task('beginClean', function() {
+    return del(['./app/assets/images/sprites/'])
+});
+
+gulp.task('createSprite', function() {
+    return gulp.src('./app/assets/images/icons/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('./app/assets/images/sprites/'));
+});
+
+gulp.task('copySpriteCss', function() {
+    return gulp.src('./app/assets/images/sprites/css/*.css')
+        .pipe(rename('_sprite.css'))
+        .pipe(gulp.dest('./app/assets/styles/postcss/modules'))
+});
+
+gulp.task('icons', gulp.series('beginClean', 'createSprite', 'copySpriteCss'));
